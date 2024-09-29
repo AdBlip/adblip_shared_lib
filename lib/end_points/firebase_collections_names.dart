cae3f3975948b5940d7682eada281487e56ebd22
Amr-Haitham <amrofficialacc2@gmail.com>
2024-09-30 00:14:28 +0300

    changed board subscription to booking

--- lib/end_points/firebase_collections_names.dart
+++ lib/end_points/firebase_collections_names.dart
@@ -2,7 +2,7 @@ class FirestoreCollectionNames {
   static const String usersCollection = "users";
   static const String boardsCollecion = "boards";
   static const String companiesCollecion = "companies";
-  static const String boardsSubscriptionCollection = "boards_subscription";
+  static const String bookingsCollection = "bookings";
   static const String boardsInActionCollection = "boardsInAction";
   static const String boardsLocationsCollecion = "boards_locations";
   static const String searchedPlacesCollection = "searched_places";
