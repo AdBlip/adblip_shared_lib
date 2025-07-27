891f76adfb2c229693aace18669f5281588e3887
Amr Shams <amrofficialacc2@gmail.com>
2025-07-28 01:00:58 +0300

    added language codes to the shared library

---
+++ lib/models/consts/language_codes.dart
@@ -0,0 +1,6 @@
+class LanguageCodesConstants {
+  //ISO codes as described in the ISO 639-1 international standard.
+  //https://en.wikipedia.org/wiki/ISO_639-1
+  static const String arabic = "ar";
+  static const String english = "en";
+}
