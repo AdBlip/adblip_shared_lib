f381afd9e8fd7df4ce92ab783e25391e785e6531
Amr Shams <amrofficialacc2@gmail.com>
2025-07-28 02:53:29 +0300

    made country codes as enum

--- lib/models/consts/country_codes.dart
+++ lib/models/consts/country_codes.dart
@@ -1,8 +1,42 @@
-class CountryCodesConstants {
+enum CountryCodesConstants {
   //ISO codes as described in the ISO 3166 international standard.
   //https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
-  static const String egypt = "EG";
-  static const String saudiArabia = "SA";
-  static const String uae = "AE";
-  static const String kuwait = "KW";
+  egypt("EG"),
+  saudiArabia("SA"),
+  uae("AE"),
+  kuwait("KW");
+
+  const CountryCodesConstants(this.code);
+
+  final String code;
+
+  static const List<String> allCodes = ["EG", "SA", "AE", "KW"];
+
+  String get toName {
+    switch (this) {
+      case CountryCodesConstants.egypt:
+        return "Egypt";
+      case CountryCodesConstants.saudiArabia:
+        return "Saudi Arabia";
+      case CountryCodesConstants.uae:
+        return "United Arab Emirates";
+      case CountryCodesConstants.kuwait:
+        return "Kuwait";
+    }
+  }
+
+  static String? codeToCountryName(String code) {
+    switch (code.toUpperCase()) {
+      case "EG":
+        return "Egypt";
+      case "SA":
+        return "Saudi Arabia";
+      case "AE":
+        return "United Arab Emirates";
+      case "KW":
+        return "Kuwait";
+      default:
+        return null;
+    }
+  }
 }
