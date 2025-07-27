3cc32353f900a57c9dcf0cb4802d8cedb32e382f
Amr Shams <amrofficialacc2@gmail.com>
2025-07-27 22:53:55 +0300

    added country codes

--- lib/models/consts/country_codes.dart
+++ lib/models/consts/country_codes.dart
@@ -1,4 +1,6 @@
-class CountryCodes {
+class CountryCodesConstants {
+  //ISO codes as described in the ISO 3166 international standard.
+  //https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
   static const String egypt = "EG";
   static const String saudiArabia = "SA";
   static const String uae = "AE";
