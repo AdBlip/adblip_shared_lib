51626209a3b53c906ba90ad527841482f31672f4
Amr Shams <amrofficialacc2@gmail.com>
2025-08-10 00:23:00 +0300

    edited booking class

--- lib/models/core_models/booking.dart
+++ lib/models/core_models/booking.dart
@@ -5,13 +5,29 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'ad.dart';
 
-enum BookingState { pending, approved, rejected, canceled }
+enum BookingState {
+  pending,
+  approved,
+  rejected,
+  canceled;
+
+  String toJson() => switch (this) {
+        pending => 'pending',
+        approved => 'approved',
+        rejected => 'rejected',
+        canceled => 'canceled',
+      };
+
+  factory BookingState.fromJson(String json) =>
+      BookingState.values.firstWhere((e) => e.toJson() == json);
+}
 
 class Booking {
   final String id;
   final String boardId;
+  final bool isAddedByBoardOwner;
   final String boardOwnerId;
-  final String customerId;
+  final String clientId;
   final BookingState bookingState;
   final Timestamp startDate;
   final Timestamp endDate;
@@ -22,8 +38,9 @@ class Booking {
   Booking({
     required this.id,
     required this.boardId,
+    required this.isAddedByBoardOwner,
     required this.boardOwnerId,
-    required this.customerId,
+    required this.clientId,
     required this.bookingState,
     required this.startDate,
     required this.endDate,
@@ -36,8 +53,9 @@ class Booking {
   Booking copyWith({
     String? id,
     String? boardId,
+    bool? isAddedByBoardOwner,
     String? boardOwnerId,
-    String? customerId,
+    String? clientId,
     BookingState? bookingState,
     Timestamp? startDate,
     Timestamp? endDate,
@@ -51,8 +69,9 @@ class Booking {
     return Booking(
       id: id ?? this.id,
       boardId: boardId ?? this.boardId,
+      isAddedByBoardOwner: isAddedByBoardOwner ?? this.isAddedByBoardOwner,
       boardOwnerId: boardOwnerId ?? this.boardOwnerId,
-      customerId: customerId ?? this.customerId,
+      clientId: clientId ?? this.clientId,
       bookingState: bookingState ?? this.bookingState,
       startDate: startDate ?? this.startDate,
       endDate: endDate ?? this.endDate,
@@ -67,9 +86,10 @@ class Booking {
     return <String, dynamic>{
       'id': id,
       'boardId': boardId,
+      'isAddedByBoardOwner': isAddedByBoardOwner,
       'boardOwnerId': boardOwnerId,
-      'customerId': customerId,
-      'bookingState': bookingState.index,
+      'clientId': clientId,
+      'bookingState': bookingState.toJson(),
       'startDate': startDate,
       'endDate': endDate,
       'totalPrice': totalPrice,
@@ -83,9 +103,10 @@ class Booking {
     return Booking(
       id: map['id'] as String,
       boardId: map['boardId'] as String,
+      isAddedByBoardOwner: map['isAddedByBoardOwner'] as bool,
       boardOwnerId: map['boardOwnerId'] as String,
-      customerId: map['customerId'] as String,
-      bookingState: BookingState.values[map['bookingState'] as int],
+      clientId: map['clientId'] as String,
+      bookingState: BookingState.fromJson(map['bookingState'] as String),
       startDate: (map['startDate'] as Timestamp),
       endDate: (map['endDate'] as Timestamp),
       totalPrice: map['totalPrice'] as double,
@@ -102,7 +123,7 @@ class Booking {
 
   @override
   String toString() {
-    return 'Booking(id: $id, boardId: $boardId, boardOwnerId: $boardOwnerId, customerId: $customerId, bookingState: $bookingState, startDate: $startDate, endDate: $endDate, totalPrice: $totalPrice, hasPaid: $hasPaid, currency: $currency, ad: $ad)';
+    return 'Booking(id: $id, boardId: $boardId, isAddedByBoardOwner: $isAddedByBoardOwner, boardOwnerId: $boardOwnerId, clientId: $clientId, bookingState: $bookingState, startDate: $startDate, endDate: $endDate, totalPrice: $totalPrice, hasPaid: $hasPaid, currency: $currency, ad: $ad)';
   }
 
   @override
@@ -111,8 +132,9 @@ class Booking {
 
     return other.id == id &&
         other.boardId == boardId &&
+        other.isAddedByBoardOwner == isAddedByBoardOwner &&
         other.boardOwnerId == boardOwnerId &&
-        other.customerId == customerId &&
+        other.clientId == clientId &&
         other.bookingState == bookingState &&
         other.startDate == startDate &&
         other.endDate == endDate &&
@@ -126,8 +148,9 @@ class Booking {
   int get hashCode {
     return id.hashCode ^
         boardId.hashCode ^
+        isAddedByBoardOwner.hashCode ^
         boardOwnerId.hashCode ^
-        customerId.hashCode ^
+        clientId.hashCode ^
         bookingState.hashCode ^
         startDate.hashCode ^
         endDate.hashCode ^
